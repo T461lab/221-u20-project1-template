cf5f4cd1ea1dc68a0c5b2eecb1a743fe161a20cd
Tristan Rosario <tr3883@students.necsd.net>
2025-07-10 14:14:26 -0400

    ...

--- client/public/js/feed.js
+++ client/public/js/feed.js
@@ -1,8 +1,21 @@
-let me = {firstName: 'Tristan', lastName: 'Rosario', age: 15};
-
-window.addEventListener('load',() => {
-    let context = document.getElementById("content");
-    Content.innerHTML = me.firstName; + "<br / "
-    Content.innerHTML += me.lastname; + " "
-    content.innerHTML += me.age;
-});
+function goToLocation(path) {
+  window.location.href = path;
+}
+function checkKeyPress(e) {
+  if (e.keyCode === 32) {
+    e.preventDefault();
+    goToLocation('http://my.marist.edu');
+  }
+}
+window.onload = function () {
+  document.body.setAttribute("tabindex", "0");
+  document.body.focus();
+  document.body.addEventListener("keydown", checkKeyPress);
+  const clickTarget = document.getElementById('portal_button');
+  if (clickTarget) {
+    clickTarget.style.cursor = 'pointer';
+    clickTarget.addEventListener('click', () => {
+      goToLocation('http://my.marist.edu');
+    });
+  }
+};
--- client/public/js/index.js
+++ client/public/js/index.js
@@ -1,11 +1,21 @@
-
-
-
-/*
- * Check ascii code for spacebar and call goToLocation in global to change URL
- */
+function goToLocation(path) {
+  window.location.href = path;
+}
 function checkKeyPress(e) {
-  if (e.keyCode == 32) {
+  if (e.keyCode === 32) {
+    e.preventDefault();
     goToLocation('/feed');
   }
 }
+window.onload = function () {
+  document.body.setAttribute("tabindex", "0");
+  document.body.focus();
+  document.body.addEventListener("keydown", checkKeyPress);
+  const clickTarget = document.getElementById('fade_text');
+  if (clickTarget) {
+    clickTarget.style.cursor = 'pointer';
+    clickTarget.addEventListener('click', () => {
+      goToLocation('/feed');
+    });
+  }
+};
